303b01d3bf0cc64978bf27406c22de9d3c06a98b
EL-FENAOUY <elfenaouyreda@gmail.com>
2022-09-13 10:51:03 +0100

    First commit

---
+++ Flask_app.py
@@ -0,0 +1,52 @@
+# Dependencies
+from os import system
+from flask import Flask, request, jsonify
+import json
+import random
+import traceback
+import pandas as pd
+import numpy as np
+import pickle
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# Your API definition
+app = Flask(__name__)
+
+#Load Dataframe
+###---------- load data -------- 
+def load_all_data():
+    
+    data = pd.read_csv("data/train_set_echantillon.csv")
+
+    return data
+
+data = load_all_data()
+
+
+@app.route('/credit/<id_client>', methods=['GET'])
+def credit(id_client):
+
+    model = pickle.load(open("C:/Users/lenovo/Flask_app/modelisation/classifier_lgbm_model.sav", 'rb'))
+
+
+
+    ID = int(id_client)
+    X = data[data['SK_ID_CURR'] == ID]
+    X = X.drop(['SK_ID_CURR', 'TARGET'], axis=1)
+    y_pred = model.predict(X)
+    y_proba = model.predict_proba(X)
+
+    dict_final = {
+        'prediction' : int(y_pred),
+        'proba' : float(y_proba[0][0])
+        }
+
+    print('Nouvelle Prédiction : \n', dict_final)
+
+    return jsonify(dict_final)
+
+
+if __name__ == "__main__":
+    app.run()
