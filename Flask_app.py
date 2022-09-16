9e52077844d3817f4059a916fbb3dedf582abc9d
EL-FENAOUY <94041017+EL-FENAOUY@users.noreply.github.com>
2022-09-16 15:43:23 +0100

    Update Flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -22,15 +22,11 @@ def load_all_data():
 
 
 
-app = Flask(__name__)
+
 @app.route('/post/<int:id>')
-def show_post(id):
+#def show_post(id):
     # Shows the post with given id.
-    return f'This post has the id {id}'
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+    #return f'This post has the id {id}'
 
 
 #@app.route("/credit")
@@ -41,27 +37,29 @@ if __name__ == "__main__":
         
         #return "Hello, World!"
 
+app = Flask(__name__)
+@app.route('/credit/<int:id_client>', methods=['GET'])
+def credit(id_client):
 
-#@app.route('/credit/<int:id_client>', methods=['GET'])
-#def credit(id_client):
+    data = load_all_data()
+    model = pickle.load(open("./modelisation/classifier_lgbm_model.sav", 'rb'))
+    ID = int(id_client)
+    X = data[data['SK_ID_CURR'] == ID]
+    X = X.drop(['SK_ID_CURR', 'TARGET'], axis=1)
+    y_pred = model.predict(X)
+    y_proba = model.predict_proba(X)
 
-    #data = load_all_data()
-    #model = pickle.load(open("./modelisation/classifier_lgbm_model.sav", 'rb'))
-    #ID = int(id_client)
-    #X = data[data['SK_ID_CURR'] == ID]
-    #X = X.drop(['SK_ID_CURR', 'TARGET'], axis=1)
-    #y_pred = model.predict(X)
-    #y_proba = model.predict_proba(X)
+    dict_final = {
+        'prediction' : int(y_pred),
+        'proba' : float(y_proba[0][0])
+        }
 
-    #dict_final = {
-        #'prediction' : int(y_pred),
-        #'proba' : float(y_proba[0][0])
-        #}
+    print('Nouvelle Prédiction : \n', dict_final)
 
-    #print('Nouvelle Prédiction : \n', dict_final)
+    return jsonify(dict_final)
 
-    #return jsonify(dict_final)
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
