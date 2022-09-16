b558289137f0b3372570e523f5c04a45828692df
EL-FENAOUY <94041017+EL-FENAOUY@users.noreply.github.com>
2022-09-16 11:35:35 +0100

    Update Flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -27,37 +27,42 @@ def load_all_data():
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_view():
+#@app.route("/")
+#def home_view():
         
-        data = load_all_data()
-        model = pickle.load(open("./modelisation/classifier_lgbm_model.sav", 'rb'))
-        return "Hello, World!"
+        #data = load_all_data()
+        #model = pickle.load(open("./modelisation/classifier_lgbm_model.sav", 'rb'))
+        #return "Hello, World!"
         
     
     
+#if __name__ == "__main__":
+        #app.run(debug=TRUE)
+        
+        
+
+
+@app.route('/credit/<id_client>', methods=['GET'])
+def credit(id_client):
+
+    data = load_all_data()
+    model = pickle.load(open("./modelisation/classifier_lgbm_model.sav", 'rb'))
+    ID = int(id_client)
+    X = data[data['SK_ID_CURR'] == ID]
+    X = X.drop(['SK_ID_CURR', 'TARGET'], axis=1)
+    y_pred = model.predict(X)
+    y_proba = model.predict_proba(X)
+
+    dict_final = {
+        'prediction' : int(y_pred),
+        'proba' : float(y_proba[0][0])
+        }
+
+    print('Nouvelle Prédiction : \n', dict_final)
+
+    return jsonify(dict_final)
+
 if __name__ == "__main__":
         app.run(debug=TRUE)
-        
-        
-
-
-#@app.route('/credit/<id_client>', methods=['GET'])
-#def credit(id_client):
-
-    #ID = int(id_client)
-    #X = data[data['SK_ID_CURR'] == ID]
-    #X = X.drop(['SK_ID_CURR', 'TARGET'], axis=1)
-    #y_pred = model.predict(X)
-    #y_proba = model.predict_proba(X)
-
-    #dict_final = {
-        #'prediction' : int(y_pred),
-        #'proba' : float(y_proba[0][0])
-        #}
-
-    #print('Nouvelle Prédiction : \n', dict_final)
-
-    #return jsonify(dict_final)
 
 
