9bc46d8d235b2036011969e255554b89e0ed0892
EL-FENAOUY <94041017+EL-FENAOUY@users.noreply.github.com>
2022-09-16 16:16:45 +0100

    Update Flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -49,15 +49,15 @@ def credit(id_client):
     y_pred = model.predict(X)
     y_proba = model.predict_proba(X)
 
-    dict_final = {
-        'prediction' : int(y_pred),
-        'proba' : float(y_proba[0][0])
-        }
+    #dict_final = {
+        #'prediction' : int(y_pred),
+        #'proba' : float(y_proba[0][0])
+        #}
 
     #print('Nouvelle Prédiction : \n', dict_final)
 
     #return jsonify(dict_final)
-    return f'This post has the id {y_pred} and {y_proba[0][0]}'
+    return f'This post has the id {y_pred}'
 
 if __name__ == "__main__":
     app.run(debug=True)
